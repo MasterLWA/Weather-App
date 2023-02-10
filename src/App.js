3f2514792b9b630ae7ed9720b10662b555dd9c60
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-02-11 02:10:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Search from './components/search/Search'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
